dbed1371d2f8c69de663a8b0a8065d5d9037ca42
Justin <justin@baal-software.com>
2019-04-14 16:43:20 -0600

    Add ability for shifter to be instantiated from class method

--- lib/shifter.rb
+++ lib/shifter.rb
@@ -1,11 +1,28 @@
-class Shifter
+require './lib/cipher'
 
+class Shifter < Cipher
+  attr_reader :keyset
 
-  def generate_keys
+  def initialize(keyset)
+    @keyset = keyset
+  end
+
+  def self.new_shifter
+    keys = generate_keys_from_rand
+    offsets = generate_offsets
+    keyset= [keys, offsets].transpose.map { |arr| arr.reduce(:+) }
+    Shifter.new(keyset)
+  end
+
+  def self.generate_keys_from_rand
     numbers = rand(100000).digits
     numbers.unshift(0) until numbers.length == 5
     numbers = numbers.each_cons(2).to_a
     numbers.map { |arr_nums| arr_nums.join.to_i }
   end
 
+  def self.generate_offsets
+    large_number = Time.now.strftime("%d%m%y").to_i ** 2
+    large_number.digits.slice(-4, 4)
+  end
 end
--- test/shifter_test.rb
+++ test/shifter_test.rb
@@ -6,16 +6,31 @@ class ShifterTest < Minitest::Test
   attr_reader :shifter
 
   def setup
-    @shifter = Shifter.new
+    @shifter = Shifter.new_shifter
   end
 
   def test_it_exists
     assert_instance_of Shifter, shifter
   end
 
+  def test_it_has_attributes
+    expected = [1,2,3,4]
+    assert_equal expected, Shifter.new(expected).keyset
+  end
+
   def test_it_can_generate_a_zero_padded_number_under_1000
-    keys = shifter.generate_keys
+    keys = Shifter.generate_keys_from_rand
     assert keys.all? { |key| key < 100 and key >= 0 }
+  end
+
+  def test_it_can_generate_offsets
+    assert Shifter.generate_offsets.all? { |key| key < 10 and key >= 0 }
+  end
+
+  def test_it_can_generate_its_own_keyset
+    assert Shifter.new_shifter.keyset.all? { |num| Integer === num }
+    assert Shifter.new_shifter.keyset.all? { |num| num >= 0 }
+    assert Shifter.new_shifter.keyset.length == 4
   end
 
 end
