8ba6d05f4556deb2dca97bfc181cbe8bff30140b
Justin <justin@baal-software.com>
2019-04-16 17:54:01 -0600

    Utilize commandline_handler class for decrypt/encrypt

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,11 +1,5 @@
-require './lib/enigma'
+require './lib/commandline_handler'
 
 message, file, key, date = ARGV
+CommandLineHandler.convert_message(:decryption, message, file, key, date)
 
-old_msg = File.open(message, "r")
-new_msg = File.open(file, "w")
-
-decrypted_message = Enigma.new.decrypt(old_msg.read, key, date)
-new_msg.write(decrypted_message[:decryption])
-new_msg.close
-p "Created #{file} with the key: #{decrypted_message[:key]} and date: #{decrypted_message[:date]}"
