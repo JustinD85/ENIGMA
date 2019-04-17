b2344f2ab146aecbe036694daae24d769bec15c3
Justin <justin@baal-software.com>
2019-04-16 18:05:43 -0600

    Refactor commandline_handler for increased readability

--- lib/commandline_handler.rb
+++ lib/commandline_handler.rb
@@ -2,14 +2,18 @@ require './lib/enigma'
 
 class CommandLineHandler
 
-  def self.convert_message(translation_method, message, file, key, date)
+  def self.convert_message(translation_method, old_file, new_file, key, date)
     encrypt_or_decrypt = translation_method == :encryption ? :encrypt : :decrypt
-    old_msg = File.open(message, "r")
-    new_msg = File.open(file, "w")
-    translated_msg = Enigma.new.send(encrypt_or_decrypt, old_msg.read, key, date)
+    old_msg = File.open(old_file, "r").read
+    new_msg = File.open(new_file, "w")
+    translated_msg = Enigma.new.send(encrypt_or_decrypt, old_msg, key, date)
     new_msg.write(translated_msg[translation_method])
     new_msg.close
-    p "Created #{file} with the key: #{translated_msg[:key]} and date: #{translated_msg[:date]}"
+    print_message(new_file, translated_msg)
+  end
+
+  def self.print_message(file, message)
+    p "Created #{file} with key: #{message[:key]} and date: #{message[:date]}"
   end
 
 end
